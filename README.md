53b74b76b75d1c91442dae3016ec1087a16a75c5
Sagun Bhandari <sagunbhandari123@gmail.com>
2024-11-27 13:23:18 +0545

    Setup tailwind and created components, Header, Body, Footer

---
+++ README.md
@@ -0,0 +1,2 @@
+- App.js is a container for all the components
+- It is a layout for structuring all the components of our react app
---
+++ src/components/Body.js
@@ -0,0 +1,9 @@
+const Body = () => {
+    return(
+        <body>
+            <h2>This is Body.</h2>
+        </body>
+    );
+};
+
+export default Body;
